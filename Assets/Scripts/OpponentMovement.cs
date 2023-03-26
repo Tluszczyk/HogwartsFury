636a8d666fb257fb35ed6156242624c27399af99
Filip Tłuszcz <tluszczykin4matyk@gmail.com>
2023-03-26 18:38:34 +0200

    Added opponent with basic movement

--- Assets/Scripts/OpponentMovement.cs
+++ Assets/Scripts/OpponentMovement.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OpponentMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
 
-    // Update is called once per frame
+    const int DEFAULT_SPEED = 1;
+
+    // Start is called before the first frame update
     void Update()
     {
-        
+        Vector3 velocity = (target.position - transform.position).normalized;
+        GetPlayerBody().velocity = NormalizeToDefaultSpeed(velocity);
+        FlipAccordingToVelocity();
+    }
+
+    private void FlipAccordingToVelocity() {
+        if (GetPlayerBody().velocity.x > 0) {
+            GetComponent<SpriteRenderer>().flipX = true;
+        } else if (GetPlayerBody().velocity.x < 0) {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+
+    Vector3 NormalizeToDefaultSpeed(Vector3 velocity) {
+        if (velocity.magnitude != 0) {
+            return velocity / (velocity.magnitude / DEFAULT_SPEED);
+        } else {
+            return velocity;
+        }
+    }
+
+    Rigidbody2D GetPlayerBody()
+    {
+        return GetComponent<Rigidbody2D>();
     }
 }
