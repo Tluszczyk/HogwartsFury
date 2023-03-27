8023dbb90cc59996aaa7dde596fe61b1968514a8
Filip Tłuszcz <tluszczykin4matyk@gmail.com>
2023-03-27 20:00:09 +0200

    mobile development added

--- Assets/Scripts/OpponentMovement.cs
+++ Assets/Scripts/OpponentMovement.cs
@@ -4,22 +4,31 @@ public class OpponentMovement : MonoBehaviour
 {
     public Transform target;
 
-    const int DEFAULT_SPEED = 1;
+    public const int DEFAULT_SPEED = 1;
+    public const float MAX_DISTANCE = 1f;
 
     // Start is called before the first frame update
     void Update()
     {
         Vector3 velocity = (target.position - transform.position).normalized;
-        GetPlayerBody().velocity = NormalizeToDefaultSpeed(velocity);
-        FlipAccordingToVelocity();
+        velocity = NormalizeToDefaultSpeed(velocity);
+        
+        if ( !IsCloseToTarget() ) {
+            GetPlayerBody().velocity = velocity;
+        } else {
+            GetPlayerBody().velocity = Vector3.zero;
+        }
+
+        FlipAccordingToDirection();
     }
 
-    private void FlipAccordingToVelocity() {
-        if (GetPlayerBody().velocity.x > 0) {
+    private void FlipAccordingToDirection() {
+        bool isTargetToTheRight = target.position.x > transform.position.x;
+
+        if ( isTargetToTheRight )
             GetComponent<SpriteRenderer>().flipX = true;
-        } else if (GetPlayerBody().velocity.x < 0) {
-            GetComponent<SpriteRenderer>().flipX = false;
-        }
+
+        else GetComponent<SpriteRenderer>().flipX = false;
     }
 
     Vector3 NormalizeToDefaultSpeed(Vector3 velocity) {
@@ -28,6 +37,10 @@ public class OpponentMovement : MonoBehaviour
         } else {
             return velocity;
         }
+    }
+
+    bool IsCloseToTarget() {
+        return Vector3.Distance(transform.position, target.position) < MAX_DISTANCE;
     }
 
     Rigidbody2D GetPlayerBody()
